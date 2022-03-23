bbe8dc2d03594c9561c14e073ebdae2bb3b470d3
Rafael <Rafael.mattos468@gmail.com>
2022-03-23 20:18:00 -0300

    ajustes do style da calculadora

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,6 +1,25 @@
-function calculadora (oper) {
+function calcular (oper) {
    var valor1 = document.calcform.valor1.value;
-   
+   var valor2 = document.calcform.valor2.value;
+
+   if (oper == "somar") {
+      var res = parseInt(valor1) + parseInt(valor2);
+   } else {
+      if (oper == "subtrair") {
+         var res = valor1-valor2;
+      }else {
+         if (oper == "multiplicar") {
+            var res = valor1 * valor2;
+         }else {
+            if (oper == "dividir") {
+               var res = valor1 / valor2;
+            }
+         }
+
+         
+      }
+   }
+   document.calcform.res.value = res
 
     
 }
