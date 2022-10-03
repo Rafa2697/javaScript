dea2858123ca584f2dc9bb3961f8c06c56a70ad6
Rafael <Rafael.mattos468@gmail.com>
2022-10-03 00:43:18 -0300

    revisão

---
+++ desafios/d001/script.js
@@ -0,0 +1,18 @@
+
+
+let botao1 = document.getElementById("botao1")
+let botao2 = document.getElementById("botao2")
+let botao3 = document.getElementById("botao3")
+
+botao1.addEventListener("click", function(event){
+    alert('clicou em botão1')
+});
+
+botao2.addEventListener("click", function(event){
+    alert('clicou em botão2')
+});
+
+botao3.addEventListener("click", function(event){
+    alert('clicou em botão3')
+})
+
